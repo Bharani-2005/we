5b3e54813bf233c0717e9178af8c7c83a5619396
Bharani-2005 <bharanim92005@gmail.com>
2026-02-12 19:56:01 +0530

    make chnages

---
+++ script.js
@@ -0,0 +1,59 @@
+const chatgptIcon=document.getElementById("chatgptIcon")
+
+chatgptIcon.addEventListener("click",function(){
+    window.open("https://chat.openai.com", "_blank")
+})
+
+// const homeIcon=document.getElementById("homeIcon")
+
+// homeIcon.addEventListener("click",function(){
+   
+//     windows.open("index.html", "_self")
+
+// })
+
+
+const figmaIcon=document.getElementById("figmaIcon")
+
+figmaIcon.addEventListener("click",function(){
+   
+    window.open("https://www.figma.com/","_blank")
+
+})
+
+const youtubeIcon=document.getElementById("youtubeIcon")
+youtubeIcon.addEventListener("click",function(){
+    window.open("https://www.youtube.com/")
+})
+
+const githubIcon=document.getElementById("githubIcon")
+githubIcon.addEventListener("click",function(){
+    window.open("https://github.com/","_blank")
+})
+
+const linkedinIcon=document.getElementById("linkedinIcon")
+linkedinIcon.addEventListener("click",function(){
+    window.open("https://in.linkedin.com","_blank")
+})
+
+const mailIcon=document.getElementById("mailIcon")
+mailIcon.addEventListener("click",function(){
+    window.open("https://mail.google.com/","_blank")
+})
+
+const twitterIcon=document.getElementById("twitterIcon")
+twitterIcon.addEventListener("click",function(){
+    window.open("https://x.com/","_blank")
+})
+
+
+const calIcon=document.getElementById("calIcon")
+calIcon.addEventListener("click",function(){
+    window.open("https://calendar.google.com/calendar/","_blank")
+})
+
+const gearIcon=document.getElementById("gearIcon")
+gearIcon.addEventListener("click",function(){
+    window.open("https://chromewebstore.google.com/?hl=en","_blank")
+})
+
